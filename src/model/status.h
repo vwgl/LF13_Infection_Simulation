32c8c345efb622522a96d70ad210dd7ad2c1b75e
sloorx <sloorx@mailbox.org>
2024-01-29 14:07:57 +0100

    Added status defines

---
+++ src/model/status.h
@@ -0,0 +1,12 @@
+#ifndef STATUS_H
+#define STATUS_H
+
+#define DECEASED 1
+#define IMMUNE 2
+#define INFECTED 4
+#define ISOLATED 8
+#define SYMPTOMATIC 16
+#define CONTAGIOUS 32
+
+
+#endif // STATUS_H
