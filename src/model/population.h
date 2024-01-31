7b528106d7180818444b8960d9e37b19aacc12aa
sloorx <sloorx@mailbox.org>
2024-01-31 13:05:27 +0100

    added getPersons and empty constructor

--- src/model/population.h
+++ src/model/population.h
@@ -8,12 +8,14 @@
 class Population
 {
 public:
+    Population();
     Population(int x_size, int y_size, int radius, int num_persons);
     std::vector<Person*>* getSector(int idx);
     std::vector<Person*>* getSector(int x, int y);
     bool addPerson(int x, int y, int status);
     void movePerson(Person *p, int src_x, int src_y, int dest_x, int dest_y);
     void killPerson(Person *p, int x, int y);
+    std::vector<Person*>* getPersons();
     void incSickCounter();
     void decSickCounter();
     int getTotalPopulation();
@@ -24,6 +26,7 @@ public:
 
 private:
     std::vector<std::vector<Person*>*> area;
+    std::vector<Person*>* persons;
     int pers_sick;
     int pers_alive;
     int pers_total;
