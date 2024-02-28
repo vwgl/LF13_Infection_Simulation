3f0896268cc3529e6a1b9bbbc2d6258487e08d98
sloorx <sloorx@mailbox.org>
2024-02-28 12:28:45 +0100

    added infection logic

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -3,6 +3,7 @@
 
 #include "controller.h"
 #include "../view/mainwindow.h"
+#include "../model/status.h"
 
 Controller::Controller(MainWindow gui) {
     x_size =  0; //TODO
@@ -26,9 +27,11 @@ void Controller::cont()
 
 void Controller::step()
 {
-    int x,y;
+    int x,y, status;
     std::vector<Person*> *persons = population.getPersons();
     Person *p;
+    Person *o;
+    std::vector<Person *> *sector;
     srand(time(0));
     // move persons
     for(int i = 0; i < persons->size(); i++){
@@ -50,6 +53,163 @@ void Controller::step()
             y = y_size - 1;
         }
         population.movePerson(p,p->getX(),p->getY(),x,y);
+    }
+    // update status
+    for(int i = 0; i < persons->size(); i++){
+        p = persons->at(i);
+        status = p->getStatus();
+        if((status & CONTAGIOUS) == CONTAGIOUS){
+            x = p->getX();
+            y = p->getY();
+            sector = population.getSector(x,y);
+            for(int j = 0; j < sector->size(); j++){
+                o = sector->at(j);
+                status = o->getStatus();
+                if((status & IMMUNE) != IMMUNE){
+                    if((status & INFECTED) != INFECTED){
+                        status = status | INFECTED;
+                        o->updateStatus(status);
+                        population.incSickCounter();
+                    }
+                }
+            }
+            x -= params[3]; //W
+            if(x >= 0){
+                sector = population.getSector(x,y);
+                for(int j = 0; j < sector->size(); j++){
+                    o = sector->at(j);
+                    if(p->getX() - o->getX() + abs(p->getY() -  o->getY()) <= params[3]){
+                        status = o->getStatus();
+                        if((status & IMMUNE) != IMMUNE){
+                            if((status & INFECTED) != INFECTED){
+                                status = status | INFECTED;
+                                o->updateStatus(status);
+                                population.incSickCounter();
+                            }
+                        }
+                    }
+                }
+            }
+            y -= params[3]; ///NW
+            if(y >= 0 && x>= 0){
+                sector = population.getSector(x,y);
+                for(int j = 0; j < sector->size(); j++){
+                    o = sector->at(j);
+                    if(p->getX() - o->getX() + p->getY() -  o->getY() <= params[3]){
+                        status = o->getStatus();
+                        if((status & IMMUNE) != IMMUNE){
+                            if((status & INFECTED) != INFECTED){
+                                status = status | INFECTED;
+                                o->updateStatus(status);
+                                population.incSickCounter();
+                            }
+                        }
+                    }
+                }
+            }
+            x += params[3]; //N
+            if(y >= 0){
+                sector = population.getSector(x,y);
+                for(int j = 0; j < sector->size(); j++){
+                    o = sector->at(j);
+                    if(abs(p->getX() - o->getX()) + p->getY() -  o->getY() <= params[3]){
+                        status = o->getStatus();
+                        if((status & IMMUNE) != IMMUNE){
+                            if((status & INFECTED) != INFECTED){
+                                status = status | INFECTED;
+                                o->updateStatus(status);
+                                population.incSickCounter();
+                            }
+                        }
+                    }
+                }
+            }
+            x += params[3]; //NE
+            if(y >= 0 && x < x_size){
+                sector = population.getSector(x,y);
+                for(int j = 0; j < sector->size(); j++){
+                    o = sector->at(j);
+                    if(o->getX() - p->getX() + p->getY() -  o->getY() <= params[3]){
+                        status = o->getStatus();
+                        if((status & IMMUNE) != IMMUNE){
+                            if((status & INFECTED) != INFECTED){
+                                status = status | INFECTED;
+                                o->updateStatus(status);
+                                population.incSickCounter();
+                            }
+                        }
+                    }
+                }
+            }
+            y += params[3]; //E
+            if(x < x_size){
+                sector = population.getSector(x,y);
+                for(int j = 0; j < sector->size(); j++){
+                    o = sector->at(j);
+                    if(o->getX() - p->getX() + abs(p->getY() -  o->getY()) <= params[3]){
+                        status = o->getStatus();
+                        if((status & IMMUNE) != IMMUNE){
+                            if((status & INFECTED) != INFECTED){
+                                status = status | INFECTED;
+                                o->updateStatus(status);
+                                population.incSickCounter();
+                            }
+                        }
+                    }
+                }
+            }
+            y += params[3]; //SE
+            if(y < y_size && x < x_size){
+                sector = population.getSector(x,y);
+                for(int j = 0; j < sector->size(); j++){
+                    o = sector->at(j);
+                    if(o->getX() - p->getX() + o->getY() -  p->getY() <= params[3]){
+                        status = o->getStatus();
+                        if((status & IMMUNE) != IMMUNE){
+                            if((status & INFECTED) != INFECTED){
+                                status = status | INFECTED;
+                                o->updateStatus(status);
+                                population.incSickCounter();
+                            }
+                        }
+                    }
+                }
+            }
+            x -= params[3]; //S
+            if(y < y_size){
+                sector = population.getSector(x,y);
+                for(int j = 0; j < sector->size(); j++){
+                    o = sector->at(j);
+                    if(abs(p->getX() - o->getX()) + o->getY() -  p->getY() <= params[3]){
+                        status = o->getStatus();
+                        if((status & IMMUNE) != IMMUNE){
+                            if((status & INFECTED) != INFECTED){
+                                status = status | INFECTED;
+                                o->updateStatus(status);
+                                population.incSickCounter();
+                            }
+                        }
+                    }
+                }
+            }
+            x -= params[3]; //SW
+            if(x >= 0 && y < y_size){
+                sector = population.getSector(x,y);
+                for(int j = 0; j < sector->size(); j++){
+                    o = sector->at(j);
+                    if(p->getX() - o->getX() + o->getY() -  p->getY() <= params[3]){
+                        status = o->getStatus();
+                        if((status & IMMUNE) != IMMUNE){
+                            if((status & INFECTED) != INFECTED){
+                                status = status | INFECTED;
+                                o->updateStatus(status);
+                                population.incSickCounter();
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
